b1ebc4c3f2f7f3f5ade77625ea0f41c29bb12658
davidsantiagoquevedo <davidsantiagoquevedo@gmail.com>
2024-02-23 16:20:54 +0100

    feat: export mittag_leffler() and prabhakar_mittag_leffler()

---
+++ mittag_leffler/__init__.py
@@ -0,0 +1,16 @@
+"""
+Mittag-Leffler
+
+Raw implementation of the Mittag-Leffler function.
+"""
+
+__version__ = "0.0.1"
+__author__ = 'David Santiago Quevedo'
+__credits__ = 'Institute for Theoretical Physics - Utrecht University'
+
+from mittag_leffler.mittag_leffler import (
+    mittag_leffler, prabhakar_mittag_leffler
+)
+
+mittag_leffler
+prabhakar_mittag_leffler
